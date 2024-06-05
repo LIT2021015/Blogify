94063964c7841d62acadb1d78f1cf79f3eaa74ed
LIT2021015 <lit2021015@iiitl.ac.in>
2024-06-05 20:02:02 +0530

    search fixes

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -2,44 +2,33 @@ import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
+  const { searchParams } = new URL(req.url);
+  const q = searchParams.get("q");
+
   try {
-   
-    // const {searchParams}=new URL(req.url)
-    //    const q=searchParams.get("q")
-    //    console.log(q);
-      const q=""
+    const posts = await prisma.post.findMany({
+      where: {
+        OR: [
+          {
+            desc: {
+              contains: q,
+              mode: "insensitive",
+            },
+          },
+          {
+            title: {
+              contains: q,
+              mode: "insensitive",
+            },
+          },
+        ],
+      },
+    });
+    //  console.log(posts);
 
-        const posts= await prisma.post.findMany(
-          {  where:{
-
-                OR:[
-
-                    {
-                        desc:{
-
-                            contains:q,
-                             mode:'insensitive'
-                        }
-                    },
-                    {
-                         title:{
-
-                            contains:q,
-                             mode:'insensitive'
-                        }
-                    }
-
-                ]
-            }
-        }
-        )
-        //  console.log(posts);
-
-     return new NextResponse(
-        JSON.stringify(posts, { status: 200 })
-      );
+    return new NextResponse(JSON.stringify(posts, { status: 200 }));
   } catch (err) {
-    console.log("error")
+    console.log("error");
     console.log(err);
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
