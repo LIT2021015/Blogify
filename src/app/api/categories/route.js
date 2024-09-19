ed72fc712c5c129158dfe945d20efb748cdd9683
LIT2021015 <lit2021015@iiitl.ac.in>
2024-09-19 10:31:15 +0530

    api fix

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,13 +1,10 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export const GET = async () => {
   try {
-    const categories = await prisma.category.findMany(
-      
-    );
+    const categories = await prisma.category.findMany();
     // console.log(categories);
-
 
     return new NextResponse(JSON.stringify(categories, { status: 200 }));
   } catch (err) {
